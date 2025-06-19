ae305e033b1b493850d4c7e466023368ddfa6a6c
Ratchaphon1412 <ratchaphon@Ratchaphons-MacBook-Pro-3.local>
2025-06-19 22:08:08 +0700

    edit middleware jwt to Header

--- cmd/main.go
+++ cmd/main.go
@@ -32,8 +32,8 @@ func main() {
 
 	// middleware
 	auth_middleware := jwtware.New(jwtware.Config{
-		SigningKey:  jwtware.SigningKey{Key: []byte(cfg.JWT_SECRET)},
-		TokenLookup: "cookie:" + cfg.JWT_COOKIE_NAME,
+		SigningKey: jwtware.SigningKey{Key: []byte(cfg.JWT_SECRET)},
+		// TokenLookup: "cookie:" + cfg.JWT_COOKIE_NAME,
 	})
 
 	//api v1
