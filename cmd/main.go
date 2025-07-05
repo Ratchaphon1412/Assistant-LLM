287c7ed2e6869230552556eafb7229a3e21bf5d3
Ratchaphon1412 <ratchaphon@Ratchaphons-MacBook-Pro-3.local>
2025-07-05 14:01:33 +0700

    edit start with localhost

--- cmd/main.go
+++ cmd/main.go
@@ -43,5 +43,5 @@ func main() {
 	routes.AccountRouter(apiV1, auth_middleware, database.DB, &cfg)
 	routes.ChatRouter(websocketV1, auth_middleware, database.DB, &cfg)
 	routes.WeatherRouter(apiV1, auth_middleware, database.DB, &cfg)
-	app.Listen(cfg.ServerPort)
+	app.Listen(":" + cfg.ServerPort)
 }
